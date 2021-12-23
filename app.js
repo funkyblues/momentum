fed63c6848f9e4ab89c66a1a9e6804bf4d09c673
funkyblues <dnr8874@naver.com>
2021-12-23 23:37:16 +0900

    2.8 Functions part Two

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-function sayHello() {
-  console.log("Hello!");
-}
+// function sayHello(nameOfPerson, age) {
+//   console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+// }
 
-alert();
-sayHello();
-sayHello();
-sayHello();
-sayHello();
+// sayHello("changuk", 10);
+// sayHello("Hyunji", 23);
+
+// function plus(firstNumber, secondNumber) {
+//   console.log(firstNumber + secondNumber);
+// }
+
+// function divide(firstNumber, secondNumber) {
+//   console.log(firstNumber / secondNumber);
+// }
+
+// plus(2, 3);
+// divide(2, 3);
+
+const player = {
+  name: "changuk",
+  sayHello: function(otherPersonsName) {
+    console.log("hello! " + otherPersonsName + " nice to meet you!");
+  },
+};
+
+console.log(player.name);
+player.sayHello("hyunji");
