2c8e803c2ed83e9cdcf59e24725dd778444babdd
funkyblues <dnr8874@naver.com>
2021-12-24 01:56:13 +0900

    2.14 Conditionals part Two

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const age = parseInt(prompt("How old are you?")); // <-- js는 여기서 잠깐 멈춰있는 것.
 
-console.log(age);
+// console.log(isNaN(age));
+
+if (isNaN(age)) {
+  console.log("please write a number!");
+}
+else {
+  console.log(age);
+}
+
+
