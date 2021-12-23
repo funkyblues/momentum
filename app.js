6830901ac6a1c65b1cf481ac697f90108c3c33fc
funkyblues <dnr8874@naver.com>
2021-12-23 22:04:21 +0900

    2.4 Booleans

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-let a = 3;
-let b = 2;
-let myName = "wukddang";
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "changuk";
-
-console.log("your new name is " + myName);
+const amIFat = null;
+let something;
+console.log(something);
