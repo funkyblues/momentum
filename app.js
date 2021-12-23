e026289bafe1c02b06bbaff7e0d81abfcab2f2d9
funkyblues <dnr8874@naver.com>
2021-12-23 22:56:45 +0900

    2.7 Functions part One

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
-const player = {
-  name: "changuk",
-  points: 10,
-  fat: true,
-};
+function sayHello() {
+  console.log("Hello!");
+}
 
-// print
-console.log(player);
-console.log(player.name);
-console.log(player["name"]);
-
-// update
-player.fat = false;
-console.log(player);
-player.points = player.points + 15;
-
-// add
-player.lastName = "potato";
-console.log(player);
+alert();
+sayHello();
+sayHello();
+sayHello();
+sayHello();
