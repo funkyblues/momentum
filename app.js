4e26ebefb19944e56f02e4160f1a8c558c51cbc5
funkyblues <dnr8874@naver.com>
2021-12-24 01:49:47 +0900

    2.13 Conditionals

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-const calculator = {
-  add: function(a, b) {
-    return a + b;
-  },
-  minus: function(a, b) {
-    return a - b;
-  },
-  div: function(a, b) {
-    return a / b;
-  },
-  mul: function(a, b) {
-    return a * b;
-  },
-  pow: function(a, b) {
-    return a ** b;
-  },
-};
+const age = parseInt(prompt("How old are you?")); // <-- js는 여기서 잠깐 멈춰있는 것.
 
-const plusResult = calculator.add(2, 3);
-const minusResult = calculator.minus(plusResult, 10);
-const timesResult = calculator.mul(10, minusResult);
-const divideResult = calculator.div(timesResult, plusResult);
-const powerResult = calculator.pow(divideResult, minusResult);
+console.log(age);
