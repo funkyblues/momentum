563b6f475b85caa74bfda8f4a5591fa4cbbbccf8
funkyblues <dnr8874@naver.com>
2021-12-24 23:45:55 +0900

    3.6 CSS in Javascript

--- app.js
+++ app.js
@@ -1,40 +1,18 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
-console.dir(h1);
-
 function handleTitleClick() {
-  h1.style.color = "blue";
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato"; 
+  }
+  else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
+  
 }
 
- function handleMouseEnter() {
-  h1.innerText = "Mouse is here!"
-}
+h1.addEventListener("click", handleTitleClick) 
 
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!"
-}
 
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-  alert("All Good");
-}
-
-h1.addEventListener("click", handleTitleClick) //이렇게도 할 수 있음. ㅋㅋ 
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
