fbc65ff4f7b3b94ab62f183d8279ed0832d0bf6f
Changuk <dnr8874@gmail.com>
2021-12-24 10:27:58 +0900

    3.2 Searching For Elements

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const title = document.getElementById("title");
+// const hellos = document.getElementsByClassName("hello");
 
-title.innerText = "Got you!"
+// console.log(hellos);
 
-console.log(title.id);
-console.log(title.className);
+// const title = document.getElementsByTagName("h1");
+
+// //tag는 anchor, div, section, button, 같은 걸 의미!
+
+// console.log(title);
+
+
+/*이것을 자주 쓴다~*/
+/*이것은 css selector 방식*/
+/*제일 첫번째만 가져옴 ㅋㅋ*/
+// const title = document.querySelector(".hello h1");
+
+// console.log(title);
+
+// const title = document.querySelectorAll(".hello h1");
+
+// console.log(title);
+
+const title = document.querySelectorAll(".hello h1:first-child");
+
+title.innerText = "hello"
