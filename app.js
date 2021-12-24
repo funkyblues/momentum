fa209accb6684294076b65df0c5903d7b2ad48d6
funkyblues <dnr8874@naver.com>
2021-12-25 00:36:25 +0900

    3.8 CSS in Javascript part Three

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick() {
-  const clickedClass = "clicked";
-  if (h1.className === clickedClass) {
-    h1.className = "";
-  }
-  else {
-    h1.className = clickedClass;
-  }
+  // 이 기능을 toggle로 해결할 수 있음.
+  // const clickedClass = "clicked";
+  // if (h1.classList.contains(clickedClass)) {
+  //   h1.classList.remove(clickedClass);
+  // }
+  // else {
+  //   h1.classList.add(clickedClass);
+  // }
+  h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick) 
